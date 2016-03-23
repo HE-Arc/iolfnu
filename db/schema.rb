2b1c5a9939c8503400e130ecd85e4516ec1167ce
Thomas Roulin <thomasroulinCH@gmail.com>
2016-03-23 11:02:39 +0100

    L'utilisateur à un name, few ui tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323075345) do
+ActiveRecord::Schema.define(version: 20160323092150) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160323075345) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
