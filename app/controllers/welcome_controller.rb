95e4aa81eb45e14d5afb9dda7bcd63a9491d11c4
Thomas Roulin <thomasroulinCH@gmail.com>
2016-04-15 14:49:38 +0200

    Now user.name is saved in the db. So the display is working fine. Fixes #9

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
     #before_filter :authenticate_user!
 
   def index
-      
+      @last_photo = Photo.last
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-    devise_for :users
+
+    devise_for :users, :controllers => { registrations: 'registrations' }
     get 'sessions/new'
 
     # Users has many Albums that has many Photos
@@ -9,8 +10,6 @@ Rails.application.routes.draw do
             resources :photos, shallow: true
         end
     end
-
-
 
     # The priority is based upon order of creation: first created -> highest priority.
     # See how all your routes lay out with "rake routes".
