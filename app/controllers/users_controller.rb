6b4b50725d4e3eff7f35948b506277b356d03356
Yoan Blanc <yoan@dosimple.ch>
2016-04-19 18:06:09 +0200

    Test fixes
    * SMTP production configuration
    * comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,9 @@ class UsersController < ApplicationController
 
     # GET /users/{id}
     def show
+        # FIXME: et si je ne suis pas loggué?
         @user = current_user
+        # FIXME: problème du SELECT N+1
         @albums = @user.albums
     end
 
